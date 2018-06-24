01c941e97a295d8e98256ada15722ee04907041f
Simon Planberger <simon.planberger@ecx.io>
2018-06-24 13:40:51 +0200

    Initial calc multi microservice as nodejs app

---
+++ app.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var express = require('express');
+const bodyParser = require('body-parser');
+
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/', (req, res) => {
+    console.log('Request Body:\n', JSON.stringify(req.body, null, 4));
+    const muliplier = req.body.muliplier;
+    const multiplicand = req.body.multiplicand;
+    const product = muliplier * multiplicand;
+    res.header('Content-Type', 'application/json');
+    res.send(JSON.stringify({ result: product }));
+  });
+
+var port = process.env.PORT || 8080;
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
+  });
+
+module.exports = app;
